e6b365ae420b4a5b05e6364b8319c002d3357191
Isiyok <muhammed.isiyok@siemens.com>
2023-03-08 21:36:33 +0300

    till 61: SafeAreaView added

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,26 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 function GameScreen() {
     return (
-        <Text>Game Screen!</Text>
+        <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            {/* GUESS */}
+            <View>
+                <Text>Higher or Lower?</Text>
+                {/* + - */}
+            </View>
+            {/* <View>
+                LOG ROUNDS
+            </View> */}
+        </View>
     );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24
+    }
+});
